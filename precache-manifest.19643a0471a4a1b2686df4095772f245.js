52226f622eff60a710999fc9caafb1e470efc73b
Kim Anh Nguyen <kimanhou@gmail.com>
2020-10-03 18:23:25 +0200

    Updates

---
+++ precache-manifest.19643a0471a4a1b2686df4095772f245.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "13d4efaf00bcba1ce93e",
+    "url": "./static/css/main.5882f2ee.chunk.css"
+  },
+  {
+    "revision": "13d4efaf00bcba1ce93e",
+    "url": "./static/js/main.13d4efaf.chunk.js"
+  },
+  {
+    "revision": "9ee2bac8e8e8a44de14d",
+    "url": "./static/js/1.9ee2bac8.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "3b218a0dce7f385fa66a37560222aa98",
+    "url": "./index.html"
+  }
+];
