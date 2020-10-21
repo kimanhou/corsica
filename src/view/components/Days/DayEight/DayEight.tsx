79d58237fc6f5824924a395f0c372df453970891
Kim Anh Nguyen <kimanhou@gmail.com>
2020-10-21 16:26:07 +0200

    Write day 8

--- src/view/components/Days/DayEight/DayEight.tsx
+++ src/view/components/Days/DayEight/DayEight.tsx
@@ -9,12 +9,44 @@ interface IDayEightProps {
 }
 
 const DayEight : React.FC<IDayEightProps> = props => {
-    const introText = <p><Text english="" french="" /></p>;
-    const detailsMorning = <p>
-        <Text english="" french="" />
-        <b><Text english="" french="" /></b>
-        <Text english="" french="" />
-    </p>
+    const introText = 
+    <p>
+        <Text english="A hike that starts directly from the city center of Corte! More accessible and with less hills, this walk will take you to " french="Une randonnée qui part directement du centre ville de Corte ! Plus accessible et avec moins de dénivelé, cette balade vous permettra de vous conduira jusqu'à la " />
+        <b><Text english="Rossolino Bridge" french="passerelle de Rossolino" /></b>
+        <Text english=", where you will be able to swim, in water certainly very cold. This trail is also (and weirdly) much less traveled than the one leading to Lake Melo in the Restonica Valley. It is also completely free!" french=", où vous pourrez vous baigner, dans une eau certes très froide. Ce sentier est également (et biazrrement) beaucoup moins fréquenté que celui menant au lac de Melo dans la vallée de la Restonica. Il est aussi totalement gratuit !" />
+    </p>;
+    const detailsMorning = 
+    <div>
+        <p>
+            <Text english="From the " french="À partir du  " />
+            <a href="https://goo.gl/maps/PSLiSMKi4wS6Aqbr5" target='_blank'><b><Text english="starting point" french="point de départ" /></b></a>
+            <Text english=" of the hike, follow the sign" french=" de la randonnée, suivre les panneaux " />
+            <i><Text english="" french="« Ponte di u Russulinu / Refuge de la Sega »" /></i>
+            <Text english=". The markup is orange." french=". Le balisage est orange." />
+            <br></br>
+            <Text english="It takes between 2h and 2h30 to get to " french="Il faut compter entre 2 heures et 2h30 de marche pour atteindre la " />
+            <a href="https://goo.gl/maps/Mq5TscoRL2GEHneTA" target='_blank'><b><Text english="Rossolino Bridge" french="passerelle de Rossolino" /></b></a>
+            <Text english=". The path simply runs along the river below, offering magnificent views over the valley. This hike does not present any particular difficulty, you go up slowly without encountering big climbs." french=". Le sentier longe tout simplement le fleuve en contre bas, offrant de magnifiques vues sur la vallée. Cette randonnée ne présente aucune difficulté particulière, on monte tranquillement sans rencontrer de gros dénivelés." />
+            <br></br>
+            <Text english="Once at the bridge, there is a pebble beach located just below, that allows you to enjoy a cool swim. It is also a very nice place to have a picnic." french="Une fois arrivé à la passerelle, une plage de galets située juste en dessous permet de profiter d'une baignade bien fraîche. C'est également un très bel endroit pour pique-niquer." />
+            <br></br>
+            <Text english="If you want to go further, the " french="Pour ceux qui veulent aller plus loin, le " />
+            <a href="https://goo.gl/maps/7sjL5GY8uC84xxh58" target='_blank'><Text english="Sega refuge" french="refuge de la Sega" /></a>
+            <Text english=" is a 2 hour walk from Rossolino footbridge." french=" se trouve à 2h de marche de la passerelle de Rossolino." />
+            <br></br>
+            <Text english="The return is made by the same path. Walking very fast, it took us 1h45 from Rossolino Bridge to the starting point of the hike." french="Le retour se fait par le même chemin. En marchant très vite, nous avons mis 1h45 de la passerelle de Rossolino au point de départ de la randonnée." />
+            <br></br>
+            <Text english="If you don't want to go back the same way, it is also possible to make a loop through " french="Si vous ne voulez pas reprendre le même chemin, il est possible de faire une boucle par la " />
+            <a href="https://goo.gl/maps/WPcEAUQz8UhfBT7N8" target='_blank'><i><Text english="Bergerie de Conia" french="Bergerie de Conia" /></i></a>
+            <Text english=" (Conia sheephold) and " french=" et " />
+            <a href="https://goo.gl/maps/hyVoUUBsgXoGp6266" target='_blank'><Text english="the Arch (" french="l'Arche de Corte (" />
+            <i><Text english="Arcu di u Scandulaghiu" french="Arcu di u Scandulaghiu" /></i></a>
+            <Text english=") but it's way longer (about 5h30 for the return leg)." french=") mais c'est beaucoup plus long (environ 5h30 pour ce trajet de retour)." />
+        </p>
+        <IconAndText englishText="0 min from Corte" frenchText="0 min depuis le centre de Corte" iconSrc="./icons/car.png"/>
+        <IconAndText englishText="0€" frenchText="0€" iconSrc="./icons/parking.png"/>
+        <IconAndText englishText="4h30 return trip to Rossolino Bridge (outbound 2h30, inbound 2h)" frenchText="4h30 A/R jusqu'à la passerelle de Rossolino (2h30 aller, 2h retour)" iconSrc="./icons/hike.png"/>
+    </div>
     const detailsAfternoon = <div>
         <p>
             <b><Text english="" french="" /></b>
@@ -26,24 +58,24 @@ const DayEight : React.FC<IDayEightProps> = props => {
         </p>
     </div>
     const detailsUseful = <div>
-        <img src="./icons/car.png"></img>
+        <a href="https://goo.gl/maps/PSLiSMKi4wS6Aqbr5" target='_blank'><img src="./icons/parking.png"></img></a>
         <p>
-            <a href="https://goo.gl/maps/bNLjujjpA3UJ73Ch8" target='_blank'><Text english="35 min, 16 km from Corte" french="35 min, 16 km de route depuis Corte" /></a>
+            <a href="https://goo.gl/maps/PSLiSMKi4wS6Aqbr5" target='_blank'><b><Text english="Free parking" french="Parking gratuit" /></b></a>
+            <Text english=" at the starting point of the hike. Unfortunately, there are only 4 parking spots." french=" au point de départ de la randonnée. Malheureusement, il n'y a que 4 places." />
         </p>
+        <a href="https://goo.gl/maps/PSLiSMKi4wS6Aqbr5" target='_blank'><img src="./icons/map-marker-no-border.png"></img></a>
         <br></br>
-        <br></br>
-        <a href="https://goo.gl/maps/M8fCVAZu5W5Gtmzi9" target='_blank'><img src="./icons/parking.png"></img></a>
         <p>
-            <a href="https://goo.gl/maps/M8fCVAZu5W5Gtmzi9" target='_blank'><b><Text english="Parking" french="Parking" /></b></a>
-            <Text english=" of " french=" de la bergerie de Grotelle 6€ / voiture, 3€ / moto" />
-            <i><Text english=" bergerie de Grotelle" french="" /></i>
-            <Text english=" (Grotelle sheephold) 6€ / car, 3€ / motorcycle" french=""/>
+            <Text english="If you can't park there, the " french="S'il n'y a plus de place, le " />
+            <a href="https://goo.gl/maps/iKzVJPQwzdPiwvkf8" target='_blank'><b><Text english="university car park" french="parking de l'université" /></b></a>
+            <Text english=" is a good option : it's free and 15 min walk away, however you will have to climb some stairs !" french=" est une bonne option de secours. Il est gratuit et situé à 15 min de marche, mais il faut monter !" />
         </p>
+        <a href="https://goo.gl/maps/iKzVJPQwzdPiwvkf8" target='_blank'><img src="./icons/map-marker-no-border.png"></img></a>
         <br></br>
         <br></br>
-        <a href="https://www.visorando.com/randonnee-restonica-lac-melo-lac-capitello/" target='_blank'><img src="./icons/hike.png"></img></a>
+        <a href="https://www.visorando.com/randonnee-gorges-du-tavignano-et-la-passerelle-de-/" target='_blank'><img src="./icons/hike.png"></img></a>
         <p>
-            <a href="https://www.visorando.com/randonnee-restonica-lac-melo-lac-capitello/" target='_blank'><b><Text english="Visorando link" french="Lien Visorando" /></b></a>
+            <a href="https://www.visorando.com/randonnee-gorges-du-tavignano-et-la-passerelle-de-/" target='_blank'><b><Text english="Visorando link" french="Lien Visorando" /></b></a>
             <Text english=" of the hike" french=" de la randonnée" />
         </p>
         <br></br>
@@ -51,15 +83,11 @@ const DayEight : React.FC<IDayEightProps> = props => {
         <p>
             <b><Text english="Altitudes : " french="Les altitudes :" /></b>
             <br></br>
-            <Text english="1376m - " french="1376m - Parking de la bergerie de Grottelle" />
-            <i><Text english="Bergerie de Grotelle" french="" /></i>
-            <Text english=" car park" french="" />
+            <Text english="399m - University car park" french="399m - Parking de l'université" />
             <br></br>
-            <Text english="1499m - Former sheephold of Melo" french="1499m - Ancienne bergerie de Melo" />
+            <Text english="454m - Starting point of the hike" french="454m - Point de départ de la randonnée" />
             <br></br>
-            <Text english="1710m - Lake Melo" french="1710m - Lac de Melo" />
-            <br></br>
-            <Text english="1941m - Lake Capitello" french="1941m - Lac de Capitello" />
+            <Text english="781m - Rossolino Bridge" french="781m - Passerelle de Rossolino" />
         </p>
     </div>
 
@@ -71,7 +99,7 @@ const DayEight : React.FC<IDayEightProps> = props => {
                  introMap={<Map/>}
                  isFullDay={true}
                  detailsMorning={detailsMorning} detailsAfternoon={detailsAfternoon} detailsUseful={detailsUseful}
-                 programEnglishMorning={""} programFrenchMorning={""} 
+                 programEnglishMorning={"to discover the Tavignano gorges by getting to Rossolino Bridge."} programFrenchMorning={"de découvrir les gorges du Tavignano en allant jusqu'à la passerelle de Rossolino."} 
                  programEnglishAfternoon={""} programFrenchAfternoon={""}
             />
         </div>
--- src/view/components/Days/DaySeven/DaySeven.tsx
+++ src/view/components/Days/DaySeven/DaySeven.tsx
@@ -20,59 +20,65 @@ const DaySeven : React.FC<IDaySevenProps> = props => {
         <b><Text english="hiking to Lake Capitello in the Restonica Valley" french="la randonnée au lac de Capitello dans la vallée de la Restonica" /></b>
         <Text english="." french="." />
     </p>;
-    const detailsMorning = <p>
-        <h2><Text english="Getting to the starting point" french="Se rendre au point de depart" /></h2>
-        <br></br>
-        <Text english="The starting point of the hike is 16 km southwest of Corte town center. The road is in good condition but winding and sometimes very narrow, nothing overwhelming if you're cautious. " french="Le point de départ de la randonnée se situe à 16 km au sud ouest du centre ville de Corte. La route est en bon état mais sinueuse et parfois très étroite, rien d'insurmontable avec de la prudence. " />
-        <a href="https://goo.gl/maps/AM7j7pmxRnNvFJ7E9" target='_blank'><Text english="At the end of the road" french="Au bout de la route" /></a>
-        <Text english=", you will be greeted by officials and you will have to pay the " french=", on vous fera payer le " />
-        <b><Text english="parking fee (6€ per car, 3€ per motorcycle)" french="tarif d'entrée de 6€ par voiture" /></b>
-        <Text english=" there, then you will be shown where to park. This process is used to optimise parking space. The later you arrive, the further down you have to park, which means the more you have to walk up to get to the starting point of the hike, except for motorcycles because they have a dedicated parking spot next to the " french=" (3€ pour les motos) et on vous indiquera où vous garer. Cela permet d'optimiser les places de parking. Plus vous arrivez tard, plus vous devez vous garer bas et donc plus vous avez à monter à pied, sauf les motos qui ont un emplacement réservé tout en haut, à côté de la " />
-        <a href="https://goo.gl/maps/cysGQsRk3gfbjMqm6" target='_blank'><Text english="" french="bergerie de Melo" /><i><Text english="bergerie de Melo" french="" /></i></a>
-        <Text english="." french="." />
-        <br></br>
-        <Text english="There is also a shuttle bus, which departs from the " french="Une navette est également à la diposition des visiteurs depuis le " />
-        <a href="https://goo.gl/maps/AM7j7pmxRnNvFJ7E9" target='_blank'><Text english="information point" french="point d'information" /></a>
-        <Text english=" located 5 km from the entrance of the valley (on Corte's side). It is only supposed to work in August but we saw it pass in September so it is better to inquire before taking it (the infos " french=" situé à 5 km de l'entrée de la vallée (côté Corte). Elle n'est censée fonctionner qu'au mois d'août mais nous l'avons vue passer en septembre donc mieux se renseigner avant de l'emprunter (les infos " />
-        <a href="https://www.mairie-corte.fr/Vallee_de_la_Restonica_page_178_1,669.htm" target='_blank'><Text english="here" french="ici" /></a>
-        <Text english=".)" french=")." />
-        <br></br>
-        <Text english="Between the car park and the old sheepfold of Melo, you can buy regional products and snacks. A refreshment bar marks the start of the hike." french="Entre le parking et l'ancienne bergerie de Melo, il y a possibilité d'acheter des produits régionaux et de se restaurer rapidement. Une buvette marque le début de la randonnée." />
-        <br></br>
-        <br></br>
-        <br></br>
-        <h2><Text english="Lake Melo" french="Lac de Melo" /></h2>
-        <br></br>
-        <Text english="It's a " french="Il faut " />
-        <b><Text english="1h15" french="1h15" /></b>
-        <Text english=" hike to get to " french=" pour atteindre le " />
-        <a href="https://goo.gl/maps/gkUGAPB5TzUeZbLH8" target='_blank'><Text english="Lake Melo" french="lac de Melo" /></a>
-        <Text english=". It obviously goes up, however " french=". Le chemin grimpe évidemment mais ne présente " />
-        <b><Text english="nothing too difficult" french="pas de difficulté majeure" /></b>
-        <Text english=". Some passages require the use of two hands and the end of the asension has ladders and metal ropes. However, there is nothing to worry about, even for children and older people in good physical shape." french=". Certains passages nécessitent de se servir de deux mains et la fin de l'asension comporte des échelles et des cordes métalliques. Cependant rien d'affolant, même pour des enfants et des personnes plus âgées en bonne forme physique." />
-        <br></br>
-        <Text english="Lake Melo is a great place to have a picnic. Choose a flat rock on the water's edge and enjoy the view, before heading to Lake Capitello if you still have energy." french="Le lac de Melo est un bel endroit où pique-niquer. Choisissez vous un rocher plat au bord de l'eau et profitez de la vue, avant de repartir vers le lac de Capitello si vous avez encore de l'énergie." />
-        <br></br>
-        <br></br>
-        <br></br>
-        <h2><Text english="Lake Capitello" french="Lac de Capitello" /></h2>
-        <br></br>
-        <Text english="From Lake Melo to " french="Du lac de Melo au " />
-        <a href="https://goo.gl/maps/gkUGAPB5TzUeZbLH8" target='_blank'><Text english="Lake Capitello" french="lac de Capitello" /></a>
-        <Text english=", it's a " french=", il faut compter " />
-        <b><Text english="45 min" french="45 min" /></b>
-        <Text english="hike, " french=" de grimpette, " />
-        <b><Text english="this part is harder and steeper" french="cette portion est plus dure et plus raide" /></b>
-        <Text english=". It is more like climbing than hiking, however we ran into children there. Your efforts will be rewarded with a magnificent view of the lake." french=". Elle s'apparente plus à de l'escalade que de la randonnée, cependant nous y avons croisé des enfants. Les efforts sont récompensés par une magnifique vue sur le lac." />
-        <br></br>
-        <Text english="It is colder and windier there than during the trip. We saw someone swimming there, however the water is really cold. The GR20 passes behind Lake Capitello and offers a viewpoint with the two lakes one behind the other." french="Il y fait plus froid et plus venteux que durant le trajet. Nous avons vu quelqu'un s'y baigner, cependant l'eau est vraiment très froide. Le GR20 passe derrière le lac de Capitello et offre un point de vue comportant les deux lacs l'un derrière l'autre." />
-        <br></br>
-        <br></br>
-        <br></br>
-        <h2><Text english="Way back" french="Retour" /></h2>
-        <br></br>
-        <Text english="The return is made by the same way as the outward journey. Walking very quickly, it took us 1h25 to reach the car park from Lake Capitello." french="Le retour se fait par le même chemin que l'aller. En marchant très vite, nous avons mis 1h25 à rejoindre le parking depuis le lac de Capitello." />
-    </p>
+    const detailsMorning = 
+    <div>
+        <p>
+            <h2><Text english="Getting to the starting point" french="Se rendre au point de depart" /></h2>
+            <br></br>
+            <Text english="The starting point of the hike is 16 km southwest of Corte town center. The road is in good condition but winding and sometimes very narrow, nothing overwhelming if you're cautious. " french="Le point de départ de la randonnée se situe à 16 km au sud ouest du centre ville de Corte. La route est en bon état mais sinueuse et parfois très étroite, rien d'insurmontable avec de la prudence. " />
+            <a href="https://goo.gl/maps/AM7j7pmxRnNvFJ7E9" target='_blank'><Text english="At the end of the road" french="Au bout de la route" /></a>
+            <Text english=", you will be greeted by officials and you will have to pay the " french=", on vous fera payer le " />
+            <b><Text english="parking fee (6€ per car, 3€ per motorcycle)" french="tarif d'entrée de 6€ par voiture" /></b>
+            <Text english=" there, then you will be shown where to park. This process is used to optimise parking space. The later you arrive, the further down you have to park, which means the more you have to walk up to get to the starting point of the hike, except for motorcycles because they have a dedicated parking spot next to the " french=" (3€ pour les motos) et on vous indiquera où vous garer. Cela permet d'optimiser les places de parking. Plus vous arrivez tard, plus vous devez vous garer bas et donc plus vous avez à monter à pied, sauf les motos qui ont un emplacement réservé tout en haut, à côté de la " />
+            <a href="https://goo.gl/maps/cysGQsRk3gfbjMqm6" target='_blank'><Text english="" french="bergerie de Melo" /><i><Text english="bergerie de Melo" french="" /></i></a>
+            <Text english="." french="." />
+            <br></br>
+            <Text english="There is also a shuttle bus, which departs from the " french="Une navette est également à la diposition des visiteurs depuis le " />
+            <a href="https://goo.gl/maps/AM7j7pmxRnNvFJ7E9" target='_blank'><Text english="information point" french="point d'information" /></a>
+            <Text english=" located 5 km from the entrance of the valley (on Corte's side). It is only supposed to work in August but we saw it pass in September so it is better to inquire before taking it (the infos " french=" situé à 5 km de l'entrée de la vallée (côté Corte). Elle n'est censée fonctionner qu'au mois d'août mais nous l'avons vue passer en septembre donc mieux se renseigner avant de l'emprunter (les infos " />
+            <a href="https://www.mairie-corte.fr/Vallee_de_la_Restonica_page_178_1,669.htm" target='_blank'><Text english="here" french="ici" /></a>
+            <Text english=".)" french=")." />
+            <br></br>
+            <Text english="Between the car park and the old sheepfold of Melo, you can buy regional products and snacks. A refreshment bar marks the start of the hike." french="Entre le parking et l'ancienne bergerie de Melo, il y a possibilité d'acheter des produits régionaux et de se restaurer rapidement. Une buvette marque le début de la randonnée." />
+            <br></br>
+            <br></br>
+            <br></br>
+            <h2><Text english="Lake Melo" french="Lac de Melo" /></h2>
+            <br></br>
+            <Text english="It's a " french="Il faut " />
+            <b><Text english="1h15" french="1h15" /></b>
+            <Text english=" hike to get to " french=" pour atteindre le " />
+            <a href="https://goo.gl/maps/gkUGAPB5TzUeZbLH8" target='_blank'><Text english="Lake Melo" french="lac de Melo" /></a>
+            <Text english=". It obviously goes up, however " french=". Le chemin grimpe évidemment mais ne présente " />
+            <b><Text english="nothing too difficult" french="pas de difficulté majeure" /></b>
+            <Text english=". Some passages require the use of two hands and the end of the asension has ladders and metal ropes. However, there is nothing to worry about, even for children and older people in good physical shape." french=". Certains passages nécessitent de se servir de deux mains et la fin de l'asension comporte des échelles et des cordes métalliques. Cependant rien d'affolant, même pour des enfants et des personnes plus âgées en bonne forme physique." />
+            <br></br>
+            <Text english="Lake Melo is a great place to have a picnic. Choose a flat rock on the water's edge and enjoy the view, before heading to Lake Capitello if you still have energy." french="Le lac de Melo est un bel endroit où pique-niquer. Choisissez vous un rocher plat au bord de l'eau et profitez de la vue, avant de repartir vers le lac de Capitello si vous avez encore de l'énergie." />
+            <br></br>
+            <br></br>
+            <br></br>
+            <h2><Text english="Lake Capitello" french="Lac de Capitello" /></h2>
+            <br></br>
+            <Text english="From Lake Melo to " french="Du lac de Melo au " />
+            <a href="https://goo.gl/maps/gkUGAPB5TzUeZbLH8" target='_blank'><Text english="Lake Capitello" french="lac de Capitello" /></a>
+            <Text english=", it's a " french=", il faut compter " />
+            <b><Text english="45 min" french="45 min" /></b>
+            <Text english=" hike, " french=" de grimpette, " />
+            <b><Text english="this part is harder and steeper" french="cette portion est plus dure et plus raide" /></b>
+            <Text english=". It is more like climbing than hiking, however we ran into children there. Your efforts will be rewarded with a magnificent view of the lake." french=". Elle s'apparente plus à de l'escalade que de la randonnée, cependant nous y avons croisé des enfants. Les efforts sont récompensés par une magnifique vue sur le lac." />
+            <br></br>
+            <Text english="It is colder and windier there than during the trip. We saw someone swimming there, however the water is really cold. The GR20 passes behind Lake Capitello and offers a viewpoint with the two lakes one behind the other." french="Il y fait plus froid et plus venteux que durant le trajet. Nous avons vu quelqu'un s'y baigner, cependant l'eau est vraiment très froide. Le GR20 passe derrière le lac de Capitello et offre un point de vue comportant les deux lacs l'un derrière l'autre." />
+            <br></br>
+            <br></br>
+            <br></br>
+            <h2><Text english="Way back" french="Retour" /></h2>
+            <br></br>
+            <Text english="The return is made by the same way as the outward journey. Walking very quickly, it took us 1h25 to reach the car park from Lake Capitello." french="Le retour se fait par le même chemin que l'aller. En marchant très vite, nous avons mis 1h25 à rejoindre le parking depuis le lac de Capitello." />
+        </p>
+        <IconAndText englishText="35 min depuis Corte" frenchText="35 min depuis le centre de Corte" iconSrc="./icons/car.png"/>
+        <IconAndText englishText="6€" frenchText="6€" iconSrc="./icons/parking.png"/>
+        <IconAndText englishText="4h return trip at a fast pace with no breaks(1h15 to Lake Melo + 45 min to Lake Capitello)" frenchText="4h A/R à un bon rythme sans pause (1h15 jusqu'au lac de Melo + 45 min jusqu'au lac de Capitello)" iconSrc="./icons/hike.png"/>
+    </div>
     const detailsAfternoon = <div></div>
     const detailsUseful = <div>
         <img src="./icons/car.png"></img>
