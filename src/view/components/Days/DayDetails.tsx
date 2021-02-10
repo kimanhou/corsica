a6d124f61804ccd78dadb347f38827ff6e88edc5
Kim Anh Nguyen <kimanhou@gmail.com>
2021-02-10 11:02:38 +0100

    Fix dashoffset < 0 et Safari

--- src/view/components/Days/DayDetails.tsx
+++ src/view/components/Days/DayDetails.tsx
@@ -14,7 +14,30 @@ const DayDetails : React.FC<IDayDetailsProps> = props => {
     return(
         <div className={`day-details`}>
             <SectionHeader englishTitle="Details of the day" englishSubtitle="" frenchTitle="Détails de la journée" frenchSubtitle=""/>
-            <div className={`column left`}>
+            <div className={`day-details-content`}>
+                <div className={`day-details-content-left`}>
+                    {props.isFullDay && 
+                    <div className={`day-details-full-day`}>
+                        {props.morning}
+                    </div>}
+                    {!props.isFullDay && 
+                    <div className={`day-details-half-days`}>
+                        <div className={`am`}>
+                            <h2><Text english="Morning" french="Matin" /></h2>
+                            {props.morning}
+                        </div>
+                        <div className={`pm`}>
+                            <h2><Text english="Afternoon" french="Apres midi" /></h2>
+                            {props.afternoon}
+                        </div>
+                    </div>}
+                </div>
+                <div className={`day-details-content-right`}>
+                    <h2><Text english="The useful corner" french="Le cote pratique" /></h2>
+                    {props.useful}
+                </div> 
+            </div>
+            {/* <div className={`column left`}>
                 {props.isFullDay && 
                 <div className={`day-details-full-day`}>
                     {props.morning}
@@ -35,7 +58,7 @@ const DayDetails : React.FC<IDayDetailsProps> = props => {
             <div className={`column right`}>
                 <h2><Text english="The useful corner" french="Le cote pratique" /></h2>
                 {props.useful}
-            </div>
+            </div> */}
         </div>
     );
 }
